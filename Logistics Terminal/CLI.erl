faa846c49856615c801f0c7974a14e6bb1d70765
Dimitrije Jelisavcic <dimitrije.jelisavcic@orioninc.com>
2026-03-16 17:07:17 +0100

    Finished Filter and Dispatch features

--- Logistics Terminal/CLI.erl
+++ Logistics Terminal/CLI.erl
@@ -9,11 +9,11 @@ showMenu(List) ->
     io:format("--------------------------------------------------------------------~n"),
     io:format("Menu:~n"),
     io:format("1) View All:~n"),
-    io:format("2) Filter~n"),
+    io:format("2) Filter By City~n"),
     io:format("3) Stats~n"),
     io:format("4) Dispatch~n"),
     io:format("5) Exit~n"),
-
+    
     {ok, [Option]} = io:fread("", "~d"),
     showMenu(Option, List).
 
@@ -22,14 +22,14 @@ showMenu(1, List) ->
     logic:viewAll(List),
     showMenu(List);
 showMenu(2, List) ->
-    io:format("Not implemented~n"),
+    logic:filterByDestination(List),
     showMenu(List);
 showMenu(3, List) ->
     io:format("Not implemented~n"),
     showMenu(List);
 showMenu(4, List) ->
-    io:format("Not implemented~n"),
-    showMenu(List);
+    NewList = logic:dispatchAll(List),
+    showMenu(NewList);
 showMenu(5, _) ->
     io:format("Exiting app~n");
 showMenu(_, List) ->
--- Logistics Terminal/DB.erl
+++ Logistics Terminal/DB.erl
@@ -11,7 +11,18 @@ createDB() ->
         {shipment, 5, 2000.3, "Subotica", in_transit},
         {shipment, 6, 4000.9, "Zrenjanin", delivered},
         {shipment, 7, 900, "Pancevo", pending},
-        {shipment, 9, 7000.4, "Leskovac", delivered},
-        {shipment, 10, 11111.1, "Uzice", pending},
-        {shipment, 8, 1300.8, "Cacak", in_transit}
+        {shipment, 8, 7000.4, "Leskovac", delivered},
+        {shipment, 9, 11111.1, "Uzice", pending},
+        {shipment, 10, 1300.8, "Belgrade", in_transit}
     ].
+
+        % {shipment, 1, 1230.7, "Belgrade", pending},
+        % {shipment, 2, 8200.2, "Novi Sad", in_transit},
+        % {shipment, 3, 15000.3, "Nis", delivered},
+        % {shipment, 4, 6700, "Kragujevac", pending},
+        % {shipment, 5, 2000.3, "Subotica", in_transit},
+        % {shipment, 6, 4000.9, "Zrenjanin", delivered},
+        % {shipment, 7, 900, "Pancevo", pending},
+        % {shipment, 8, 7000.4, "Leskovac", delivered},
+        % {shipment, 9, 11111.1, "Uzice", pending},
+        % {shipment, 10, 1300.8, "Belgrade", in_transit}
